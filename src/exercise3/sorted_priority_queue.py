a302b898d50476c1d17ac6d44e65c92aedb0f4c1
glowing-octo-guacamole <tranthanhtoan1957@outlook.com>
2024-07-25 13:15:55 +0000

    Add inorder_next and postorder_next

---
+++ src/exercise3/sorted_priority_queue.py
@@ -0,0 +1,66 @@
+# Copyright 2013, Michael H. Goldwasser
+#
+# Developed for use with the book:
+#
+#    Data Structures and Algorithms in Python
+#    Michael T. Goodrich, Roberto Tamassia, and Michael H. Goldwasser
+#    John Wiley & Sons, 2013
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from exceptions import Empty
+from priority_queue_base import PriorityQueueBase
+
+
+class SortedPriorityQueue(PriorityQueueBase):  # base class defines _Item
+    """A min-oriented priority queue implemented with a sorted list."""
+
+    # ------------------------------ public behaviors ------------------------------
+    def __init__(self):
+        """Create a new empty Priority Queue."""
+        self._data = []
+
+    def __len__(self):
+        """Return the number of items in the priority queue."""
+        return len(self._data)
+
+    def add(self, key, value):
+        """Add a key-value pair."""
+        newest = self._Item(key, value)
+        for i, item in enumerate(self._data):
+            if newest < item:
+                self._data.insert(i, newest)
+                break
+        else:
+            self._data.append(newest)
+
+    def min(self):
+        """Return but do not remove (k,v) tuple with minimum key.
+
+        Raise Empty exception if empty.
+        """
+        if self.is_empty():
+            raise Empty("Priority queue is empty.")
+        item = self._data[0]
+        return (item._key, item._value)
+
+    def remove_min(self):
+        """Remove and return (k,v) tuple with minimum key.
+
+        Raise Empty exception if empty.
+        """
+        if self.is_empty():
+            raise Empty("Priority queue is empty.")
+        item = self._data.pop(0)
+        return (item._key, item._value)
